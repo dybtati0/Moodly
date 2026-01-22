bbc4498ec8275b1bcc44aeb3a92e5cbc76326b89
dybtati0 <dybtati0@gmail.com>
2026-01-22 22:10:26 +0200

    Delete js/main.js

--- js/main.js
+++
@@ -1,7 +0,0 @@
-const toggle = document.getElementById("toggle-dark");
-const savedDark = localStorage.getItem("dark");
-if(savedDark==="on"){document.body.classList.add("dark");if(toggle)toggle.checked=true;}
-if(toggle){toggle.addEventListener("change",()=>{
-  document.body.classList.toggle("dark");
-  localStorage.setItem("dark",document.body.classList.contains("dark")?"on":"off");
-});}
